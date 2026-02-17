fee74a87a15c7c39175462cc6a7a8026df5c64af
n1embroiderytime-sketch <n1embroiderytime@gmail.com>
2026-02-17 23:11:15 +0000

    Fix level select container by loading classic levels dynamically

--- level_select.gd
+++ level_select.gd
@@ -1,5 +1,8 @@
 extends Control
 
+@export var all_levels: Array[Resource] = []
+@export var classic_levels_path: String = "res://Gamemodes/Classic"
+
 @onready var grid_classic = $MainMargin/LayoutList/SectionClassic/ContainerClassic/LevelRow
 @onready var grid_endless = $MainMargin/LayoutList/SectionEndless/ContainerEndless/LevelRow
 @onready var mirror_row = $MainMargin/LayoutList/SectionMirror/ContainerMirror/LevelRow
@@ -9,6 +12,12 @@ func _ready():
 	_clear_container(grid_endless)
 	_clear_container(mirror_row)
 
+	all_levels = load_levels_from_folder(classic_levels_path)
+	if all_levels.is_empty():
+		all_levels = Global.game_levels.duplicate(true)
+	else:
+		Global.game_levels = all_levels.duplicate(true)
+
 	setup_endless_mode()
 	setup_classic_mode()
 	setup_mirror_mode()
@@ -16,6 +25,31 @@ func _ready():
 	var btn_back = find_child("BtnBack", true, false)
 	if btn_back and not btn_back.pressed.is_connected(_on_back_pressed):
 		btn_back.pressed.connect(_on_back_pressed)
+
+func load_levels_from_folder(path: String) -> Array[Resource]:
+	var loaded_levels: Array[Resource] = []
+	var dir := DirAccess.open(path)
+	if dir == null:
+		return loaded_levels
+
+	dir.list_dir_begin()
+	var file_name := dir.get_next()
+	var files: Array[String] = []
+	while file_name != "":
+		if not dir.current_is_dir() and (file_name.ends_with(".tres") or file_name.ends_with(".res")):
+			files.append(file_name)
+		file_name = dir.get_next()
+	dir.list_dir_end()
+
+	files.sort()
+	for f in files:
+		var full_path = path.path_join(f)
+		if ResourceLoader.exists(full_path):
+			var level_res = load(full_path)
+			if level_res:
+				loaded_levels.append(level_res)
+
+	return loaded_levels
 
 func setup_endless_mode():
 	var btn_endless = preload("res://LevelButton.gd").new()
@@ -35,8 +69,8 @@ func setup_endless_mode():
 	btn_endless.custom_minimum_size = Vector2(0, 180)
 
 func setup_classic_mode():
-	for i in range(Global.game_levels.size()):
-		var data = Global.game_levels[i]
+	for i in range(all_levels.size()):
+		var data = all_levels[i]
 		var is_locked = i > Global.highest_level_reached
 		var stars = Global.level_stars.get(i, 0)
 
